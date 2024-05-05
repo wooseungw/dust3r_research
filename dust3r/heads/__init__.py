6014d661d17814be93c2bd2310d1e3fa8deab3ee
wooseungw <woow070840@gmail.com>
2024-05-05 23:55:24 +0900

    12

--- dust3r/heads/__init__.py
+++ dust3r/heads/__init__.py
@@ -6,6 +6,7 @@
 # --------------------------------------------------------
 from .linear_head import LinearPts3d
 from .dpt_head import create_dpt_head
+from .new_head import create_new_head
 
 
 def head_factory(head_type, output_mode, net, has_conf=False):
@@ -15,5 +16,7 @@ def head_factory(head_type, output_mode, net, has_conf=False):
         return LinearPts3d(net, has_conf)
     elif head_type == 'dpt' and output_mode == 'pts3d':
         return create_dpt_head(net, has_conf=has_conf)
+    elif head_type == 'new' and output_mode == 'pts3d':
+        return create_new_head(net, has_conf=has_conf)
     else:
         raise NotImplementedError(f"unexpected {head_type=} and {output_mode=}")
